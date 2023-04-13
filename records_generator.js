0c00c01447d273ad0daee5976bf648a77cc46f43
unknown <enriconotaro@gmail.com>
2023-04-13 16:32:52 +0200

    records_generator improved

--- records_generator.js
+++ records_generator.js
@@ -1,40 +1,45 @@
 console.log('test records_generator')
-arrayIndexValue= ["PESO",1,0];
+arrayIndexValue= ["PESO",[1,2],0];
 fields=["type","value","patientId"];
 setting=[0,true];
 values= [['hr001','10kg','test','a'],['hr002','20kg','test','b'],['hr003','30kg','test','c'],['hr004','40kg','test','c']]
 
-function createRecordsForValue(fields,arrayIndexValue,setting,valuesXlsx){
+function createRecordsForValue(fields, arrayIndexValue, setting, valuesXlsx) {
   //for example,
-  //arrayIndexValue= ["PESO",159,0]
+  //arrayIndexValue= ["PESO",[1,2],0],
   //"fields"=["type","value","patientId"],
   //setting:[0,true], prendi in considerazione il vettore di valuesXlsx solo se nella pos 0 è != null
-  //values= [[..,..,..,..],[..,..,..,..]]
-
-  let valueResult='';
-  for(let rowValue of valuesXlsx){
-    let record = {};
-    if(setting!==null && rowValue[setting[0]]===null){
-      continue
+  //values= [['hr001','10kg','test','a'],['hr002','20kg','test','b'],['hr003','30kg','test','c'],['hr004','40kg','test','c']]
+  let listOfRecords=[];
+  for (const rowValue of valuesXlsx) {
+    if (setting !== null && rowValue[setting[0]] === null) {
+      continue;
     }
-    else{
-      for(let indexfield=0;indexfield<fields.length;indexfield++){
-        if(typeof arrayIndexValue[indexfield]==='string'){
-          valueResult=arrayIndexValue[indexfield]
-        }
-        else if(arrayIndexValue[indexfield].length>1){
-          for(let val of arrayIndexValue[indexfield]){
-            valueResult=valueResult+rowValue[arrayIndexValue[indexfield]];
-          }
-        }
-        else{
-          valueResult=rowValue[arrayIndexValue[indexfield]];
-        }
-
-        record[fields[indexfield]]=valueResult;
+    const record = {};
+    for (let indexField = 0; indexField < fields.length; indexField++) {
+      let valueResult = '';
+      const field = fields[indexField];
+      const indexValue = arrayIndexValue[indexField];
+      if (typeof indexValue === 'string') {
+        valueResult = indexValue;
+      } else if (indexValue.length > 1) {
+        valueResult = indexValue.map(val => rowValue[val]).join('');
+      } else {
+        valueResult = rowValue[indexValue];
       }
-      console.log(record)
+      record[field] = valueResult;
     }
+    console.log(record);
+    listOfRecords.push(record);
   }
+  return listOfRecords
 };
-createRecordsForValue(fields,arrayIndexValue,setting,values)
+//restituisce ad esempio la seguente lista
+// [
+//   { type: 'PESO', value: '10kgtest', patientId: 'hr001' },
+//   { type: 'PESO', value: '20kgtest', patientId: 'hr002' },
+//   { type: 'PESO', value: '30kgtest', patientId: 'hr003' },
+//   { type: 'PESO', value: '40kgtest', patientId: 'hr004' }
+// ]
+let lista=createRecordsForValue(fields,arrayIndexValue,setting,values);
+console.log(lista);
