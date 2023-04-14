330bff7a429bc0718145370872b2542830f3ba85
unknown <enriconotaro@gmail.com>
2023-04-14 10:42:16 +0200

    add jsonGeneratorForTable function in records_generator.js

--- records_generator.js
+++ records_generator.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
 console.log('test records_generator')
 arrayIndexValue= ["PESO",[1,2],0];
 fields=["type","value","patientId"];
 setting=[0,true];
-values= [['hr001','10kg','test','a'],['hr002','20kg','test','b'],['hr003','30kg','test','c'],['hr004','40kg','test','c']]
+values= [
+  ['hr001','10kg','test1','a','bio1','m1','pid1','161cm1','eur1','61'],
+  ['hr002','20kg','test2','b','bio2','f2','pid2','161cm2','eur2','62'],
+  ['hr003','30kg','test3','c','bio3','m3','pid3','161cm3','eur3','63'],
+  ['hr004','40kg','test4','c','bio4','f4','pid4','161cm4','eur4','64']
+]
 
 function createRecordsForValue(fields, arrayIndexValue, setting, valuesXlsx) {
   //for example,
@@ -29,9 +35,10 @@ function createRecordsForValue(fields, arrayIndexValue, setting, valuesXlsx) {
       }
       record[field] = valueResult;
     }
-    console.log(record);
+    //console.log(record);
     listOfRecords.push(record);
   }
+  //console.log(listOfRecords);
   return listOfRecords
 };
 //restituisce ad esempio la seguente lista
@@ -41,5 +48,80 @@ function createRecordsForValue(fields, arrayIndexValue, setting, valuesXlsx) {
 //   { type: 'PESO', value: '30kgtest', patientId: 'hr003' },
 //   { type: 'PESO', value: '40kgtest', patientId: 'hr004' }
 // ]
-let lista=createRecordsForValue(fields,arrayIndexValue,setting,values);
-console.log(lista);
+
+
+//let lista=createRecordsForValue(fields,arrayIndexValue,setting,values);
+//console.log(lista);
+async function readSchema() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('schema.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+        return;
+      }
+      // Analizza i dati del file JSON
+      const jsonData = JSON.parse(data);
+      // Usa i dati come necessario
+      //console.log(jsonData);
+      resolve(jsonData);
+    });
+  });
+}
+
+// console.log(Object.keys(tabella));
+// (async function main(){
+//   const res=await readSchema()
+//   //console.log(res);
+//   for(let tab of res){
+//     console.log(Object.keys(tab)[0])
+//   }
+// })()
+let array_schema=[
+  {
+    "Patient":{
+        "fields":["centerId","biobankCode","pid","code"],
+        "setting":[0,true],
+        "values":[
+            ["dd74ba73-8a99-41dd-8974-0a6912d3591a",4,6,0]
+        ]
+    }
+  },
+  {
+    "Anagrafic":{
+        "fields":["type","value","patientId"],
+        "setting":[0,true],
+        "values":[
+            ["SESSO",5,0],
+            ["PESO",1,0],
+            ["ALTEZZA",7,0],
+            ["ANNI COMPIUTI",9,0],
+            ["ETNIA",8,0],
+            ["GRADO DI ISTRUZIONE",2,0],
+        ]
+    }
+  }
+]
+
+function jsonGeneratorForTable (array_schema, excelData){
+  let recordsForTable=[];
+  for (let tab of array_schema){
+    const tableObject={};
+    const nameTable=Object.keys(tab)[0];
+    const fields=tab[nameTable]['fields'];
+    const setting=tab[nameTable]['setting'];
+    const valuesMatrix=tab[nameTable]['values'];
+    let index=1
+    let recordsList=[];
+    // Itera attraverso la matrice di valori della tabella e crea un elenco di record per ogni singolo array di valori
+    for(let valArray of valuesMatrix){
+     const listOfSingleArrayValues = createRecordsForValue(fields, valArray, setting, excelData)
+      recordsList.push(...listOfSingleArrayValues)
+    }
+    tableObject[nameTable]=recordsList;
+    recordsForTable.push(tableObject);
+  }
+  console.log(recordsForTable)
+ return recordsForTable
+}
+jsonGeneratorForTable(array_schema,values)
--- services/records_generator.js
+++ services/records_generator.js
@@ -1,33 +1,57 @@
-function createRecordsForValue(fields,arrayIndexValue,setting,valuesXlsx){
+function createRecordsForValue(fields, arrayIndexValue, setting, valuesXlsx) {
   //for example,
-  //arrayIndexValue= ["PESO",159,0]
+  //arrayIndexValue= ["PESO",[1,2],0],
   //"fields"=["type","value","patientId"],
   //setting:[0,true], prendi in considerazione il vettore di valuesXlsx solo se nella pos 0 è != null
-  //values= [[..,..,..,..],[..,..,..,..]]
-
-  let valueResult='';
-  for(let rowValue of valuesXlsx){
-    let record = {};
-    if(setting!==null && rowValue[setting[0]]===null){
-      continue
+  //values= [['hr001','10kg','test','a'],['hr002','20kg','test','b'],['hr003','30kg','test','c'],['hr004','40kg','test','c']]
+  let listOfRecords=[];
+  for (const rowValue of valuesXlsx) {
+    if (setting !== null && rowValue[setting[0]] === null) {
+      continue;
     }
-    else{
-      for(let indexfield=0;indexfield<fields.length;indexfield++){
-        if(typeof arrayIndexValue[indexfield]==='string'){
-          valueResult=arrayIndexValue[indexfield]
-        }
-        else if(arrayIndexValue[indexfield].length>1){
-          for(let val of arrayIndexValue[indexfield]){
-            valueResult=valueResult+rowValue[arrayIndexValue[indexfield]];
-          }
-        }
-        else{
-          valueResult=rowValue[arrayIndexValue[indexfield]];
-        }
-
-        record[fields[indexfield]]=valueResult;
+    const record = {};
+    for (let indexField = 0; indexField < fields.length; indexField++) {
+      let valueResult = '';
+      const field = fields[indexField];
+      const indexValue = arrayIndexValue[indexField];
+      if (typeof indexValue === 'string') {
+        valueResult = indexValue;
+      } else if (indexValue.length > 1) {
+        valueResult = indexValue.map(val => rowValue[val]).join('');
+      } else {
+        valueResult = rowValue[indexValue];
       }
-      console.log(record)
+      record[field] = valueResult;
     }
+    console.log(record);
+    listOfRecords.push(record);
   }
+  return listOfRecords
 };
+//restituisce ad esempio la seguente lista
+// [
+//   { type: 'PESO', value: '10kgtest', patientId: 'hr001' },
+//   { type: 'PESO', value: '20kgtest', patientId: 'hr002' },
+//   { type: 'PESO', value: '30kgtest', patientId: 'hr003' },
+//   { type: 'PESO', value: '40kgtest', patientId: 'hr004' }
+// ]
+function jsonGeneratorForTable (array_schema, excelData){
+  let recordsForTable=[];
+  for (let tab of array_schema){
+    const tableObject={};
+    const nameTable=Object.keys(tab)[0];
+    const fields=tab[nameTable]['fields'];
+    const setting=tab[nameTable]['setting'];
+    const valuesMatrix=tab[nameTable]['values'];
+    let index=1
+    let recordsList=[];
+    // Itera attraverso la matrice di valori della tabella e crea un elenco di record per ogni singolo array di valori
+    for(let valArray of valuesMatrix){
+     const listOfSingleArrayValues= createRecordsForValue(fields, valArray, setting, excelData)
+      recordsList.push(...listOfSingleArrayValues)
+    }
+    tableObject[nameTable]=recordsList;
+    recordsForTable.push(tableObject);
+  }
+ return recordsForTable
+}
