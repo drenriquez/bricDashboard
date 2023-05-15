952a1eca92ceb126f0c8a27c0616e39db3089186
unknown <enriconotaro@gmail.com>
2023-05-15 13:40:39 +0200

    updated component remote-database

--- src/app/remote-database/remote-database.component.ts
+++ src/app/remote-database/remote-database.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class RemoteDatabaseComponent {
 
+
+
+  onFileSelected(event: any) {
+   const selectedFile = event.target.files[0];
+
+    // Create a FileReader object
+    const fileReader = new FileReader();
+
+    // Set up an event listener for when the FileReader has loaded the file
+    fileReader.onload = (e) => {
+      const fileContent = fileReader.result as string;
+      const jsonData = JSON.parse(fileContent);
+      console.log(jsonData);
+    };
+
+    // Read the selected file as text
+    fileReader.readAsText(selectedFile);
+  }
+
+
 }
